033daf441f44a5706804171da2186b16c444e601
chengliang4810 <chengliang4810@163.com>
2025-04-30 10:52:05 +0800

    chore: 更新工作区配置、API请求设置及偏好设置
    - 在工作区配置中添加 `@vben/vant-h5` 项目路径
    - 在 `package.json` 中添加 `dev:h5` 脚本
    - 修改 `preferences.ts` 中的 `enableRefreshToken` 为 `false`
    - 更新 `request.ts` 中的 `successCode` 和错误消息字段

--- apps/web-naive/src/preferences.ts
+++ apps/web-naive/src/preferences.ts
@@ -9,5 +9,7 @@ export const overridesPreferences = defineOverridesPreferences({
   // overrides
   app: {
     name: import.meta.env.VITE_APP_TITLE,
+    // 不需要刷新token的功能
+    enableRefreshToken: false,
   },
 });
