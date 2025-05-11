b1dfad3a86c1699d2178a349f98ec55ed16b7129
chengliang4810 <chengliang4810@163.com>
2025-05-11 11:46:22 +0800

    feat(@vben/web-naive): 添加操作列和工具栏按钮
    在生成模板页面中，新增操作列包含编辑和删除按钮，并在工具栏中添加导出、删除和新增按钮，以增强用户交互功能。

--- apps/web-naive/src/views/tool/gen-template/index.vue
+++ apps/web-naive/src/views/tool/gen-template/index.vue
@@ -4,6 +4,8 @@ import type { VxeGridProps } from '#/adapter/vxe-table';
 
 import { Page } from '@vben/common-ui';
 
+import { NButton, NFlex } from 'naive-ui';
+
 // import { message } from 'naive-ui';
 import { useVbenVxeGrid } from '#/adapter/vxe-table';
 import { getGenTemplateList } from '#/api';
@@ -54,6 +56,13 @@ const gridOptions: VxeGridProps<RowType> = {
     { field: 'path', title: '生成路径' },
     { field: 'remark', title: '备注' },
     { field: 'updateTime', formatter: 'formatDateTime', title: '最后更新时间' },
+    {
+      field: 'action',
+      fixed: 'right',
+      slots: { default: 'action' },
+      title: '操作',
+      width: 150,
+    },
   ],
   keepSource: true,
   pagerConfig: {},
@@ -72,7 +81,19 @@ const gridOptions: VxeGridProps<RowType> = {
   toolbarConfig: {
     // 是否显示搜索表单控制按钮
     // @ts-ignore 正式环境时有完整的类型声明
-    // search: true,
+    custom: true,
+    // import: true,
+    refresh: true,
+    zoom: true,
+  },
+  headerCellConfig: {
+    height: 44,
+  },
+  cellConfig: {
+    height: 48,
+  },
+  rowConfig: {
+    keyField: 'userId',
   },
 };
 
@@ -81,6 +102,20 @@ const [Grid] = useVbenVxeGrid({ formOptions, gridOptions });
 
 <template>
   <Page auto-content-height>
-    <Grid />
+    <Grid>
+      <template #toolbar-tools>
+        <NFlex class="mx-3" size="small">
+          <NButton class="mr-2"> 导出 </NButton>
+          <NButton class="mr-2" type="error"> 删除 </NButton>
+          <NButton class="mr-2" type="primary"> 新增 </NButton>
+        </NFlex>
+      </template>
+      <template #action>
+        <NFlex class="mx-3" justify="space-around" size="small">
+          <NButton type="info" size="small" ghost>编辑</NButton>
+          <NButton type="error" size="small" ghost>删除</NButton>
+        </NFlex>
+      </template>
+    </Grid>
   </Page>
 </template>
