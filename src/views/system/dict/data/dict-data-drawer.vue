257d68f4fe5e2f08ab79c290cd10a4a81a5d4856
dap <15891557205@163.com>
2026-06-16 14:55:05 +0800

    refactor(dict): 重构字典表单为antdv原生组件

--- src/views/system/dict/data/dict-data-drawer.vue
+++ src/views/system/dict/data/dict-data-drawer.vue
@@ -1,19 +1,27 @@
 <script setup lang="ts">
+import type { DictData } from '@/api/system/dict/dict-data-model';
+import type { AntdFormRules } from '@/types/form';
+import type { FormInstance } from 'antdv-next';
+
 import { computed, ref } from 'vue';
 
-import { useVbenForm } from '@/adapter/form';
 import {
   dictDataAdd,
   dictDataUpdate,
   dictDetailInfo,
 } from '@/api/system/dict/dict-data';
 import { tagTypes } from '@/components/dict';
+import {
+  FormInput as Input,
+  FormInputNumber as InputNumber,
+  FormTextArea as TextArea,
+} from '@/components/global/form';
 import { useVbenDrawer } from '@/effects/common-ui';
 import { $t } from '@/locales';
 import { cloneDeep } from '@/utils';
-import { defaultFormValueGetter, useBeforeCloseDiff } from '@/utils/popup';
+import { useBeforeCloseDiff } from '@/utils/popup';
+import { Form, FormItem } from 'antdv-next';
 
-import { drawerSchema } from './data';
 import TagStylePicker from './tag-style-picker.vue';
 
 const emit = defineEmits<{ reload: [] }>();
@@ -28,17 +36,32 @@ const title = computed(() => {
   return isUpdate.value ? $t('pages.common.edit') : $t('pages.common.add');
 });
 
-const [BasicForm, formApi] = useVbenForm({
-  commonConfig: {
-    componentProps: {
-      class: 'w-full',
-    },
-    formItemClass: 'col-span-2',
-    labelWidth: 80,
-  },
-  schema: drawerSchema(),
-  showDefaultActions: false,
-  wrapperClass: 'grid-cols-2',
+type FormData = Partial<DictData>;
+
+function getDefaultValues(): FormData {
+  return {
+    cssClass: '',
+    dictCode: undefined,
+    dictLabel: '',
+    dictSort: 1,
+    dictType: '',
+    dictValue: '',
+    listClass: undefined,
+    remark: '',
+  };
+}
+
+const formData = ref<FormData>(getDefaultValues());
+const formInstance = ref<FormInstance>();
+
+const formTooltips = {
+  cssClass: '标签的css样式, 可添加已经编译的css类名',
+};
+
+const formRules = ref<AntdFormRules<FormData>>({
+  dictLabel: [{ required: true, message: $t('ui.formRules.required') }],
+  dictSort: [{ required: true, message: $t('ui.formRules.required') }],
+  dictValue: [{ required: true, message: $t('ui.formRules.required') }],
 });
 
 /**
@@ -46,7 +69,8 @@ const [BasicForm, formApi] = useVbenForm({
  * default: 预设标签样式
  * custom: 自定义标签样式
  */
-const selectType = ref('default');
+type SelectType = 'custom' | 'default';
+const selectType = ref<SelectType>('default');
 /**
  * 根据标签样式判断是自定义还是默认
  * @param listClass 标签样式
@@ -57,10 +81,14 @@ function setupSelectType(listClass: string) {
   selectType.value = isDefault ? 'default' : 'custom';
 }
 
+function customFormValueGetter() {
+  return JSON.stringify(formData.value);
+}
+
 const { onBeforeClose, markInitialized, resetInitialized } = useBeforeCloseDiff(
   {
-    initializedGetter: defaultFormValueGetter(formApi),
-    currentGetter: defaultFormValueGetter(formApi),
+    initializedGetter: customFormValueGetter,
+    currentGetter: customFormValueGetter,
   },
 );
 
@@ -76,12 +104,18 @@ const [BasicDrawer, drawerApi] = useVbenDrawer({
 
     const { dictCode, dictType } = drawerApi.getData() as DrawerProps;
     isUpdate.value = !!dictCode;
-    await formApi.setFieldValue('dictType', dictType);
+    formData.value.dictType = dictType;
 
     if (dictCode && isUpdate.value) {
       const record = await dictDetailInfo(dictCode);
-      setupSelectType(record.listClass);
-      await formApi.setValues(record);
+      setupSelectType(record.listClass ?? '');
+      formData.value = {
+        ...getDefaultValues(),
+        ...record,
+        cssClass: record.cssClass ?? '',
+        listClass: record.listClass || undefined,
+        remark: record.remark ?? '',
+      };
     }
     await markInitialized();
 
@@ -92,11 +126,8 @@ const [BasicDrawer, drawerApi] = useVbenDrawer({
 async function handleConfirm() {
   try {
     drawerApi.lock(true);
-    const { valid } = await formApi.validate();
-    if (!valid) {
-      return;
-    }
-    const data = cloneDeep(await formApi.getValues());
+    await formInstance.value?.validate();
+    const data = cloneDeep(formData.value);
     // 需要置空的情况 undefined不会提交给后端 需要改为空字符串
     if (!data.listClass) {
       data.listClass = '';
@@ -113,7 +144,8 @@ async function handleConfirm() {
 }
 
 async function handleClosed() {
-  await formApi.resetForm();
+  formData.value = getDefaultValues();
+  formInstance.value?.resetFields();
   selectType.value = 'default';
   resetInitialized();
 }
@@ -121,21 +153,52 @@ async function handleClosed() {
 /**
  * 取消标签选中 必须设置为undefined才行
  */
-async function handleDeSelect() {
-  await formApi.setFieldValue('listClass', undefined);
+function handleDeSelect() {
+  formData.value.listClass = undefined;
 }
 </script>
 
 <template>
   <BasicDrawer :title="title" :size="600">
-    <BasicForm>
-      <template #listClass="slotProps">
+    <Form
+      ref="formInstance"
+      :model="formData"
+      :label-col="{ style: { width: '80px' } }"
+    >
+      <FormItem label="字典类型" name="dictType">
+        <Input disabled class="w-full" v-model:value="formData.dictType" />
+      </FormItem>
+      <FormItem label="标签样式" name="listClass">
         <TagStylePicker
-          v-bind="slotProps"
+          v-model:value="formData.listClass"
           v-model:select-type="selectType"
           @deselect="handleDeSelect"
         />
-      </template>
-    </BasicForm>
+      </FormItem>
+      <FormItem label="数据标签" name="dictLabel" :rules="formRules.dictLabel">
+        <Input allow-clear class="w-full" v-model:value="formData.dictLabel" />
+      </FormItem>
+      <FormItem label="数据键值" name="dictValue" :rules="formRules.dictValue">
+        <Input allow-clear class="w-full" v-model:value="formData.dictValue" />
+      </FormItem>
+      <FormItem
+        label="css类名"
+        name="cssClass"
+        :tooltip="formTooltips.cssClass"
+      >
+        <TextArea
+          allow-clear
+          class="w-full"
+          placeholder="可使用tailwind类名 如bg-blue w-full h-full等"
+          v-model:value="formData.cssClass"
+        />
+      </FormItem>
+      <FormItem label="显示排序" name="dictSort" :rules="formRules.dictSort">
+        <InputNumber class="w-full" v-model:value="formData.dictSort" />
+      </FormItem>
+      <FormItem label="备注" name="remark">
+        <TextArea allow-clear class="w-full" v-model:value="formData.remark" />
+      </FormItem>
+    </Form>
   </BasicDrawer>
 </template>
--- src/views/system/dict/type/dict-type-modal.vue
+++ src/views/system/dict/type/dict-type-modal.vue
@@ -1,18 +1,24 @@
 <script setup lang="ts">
+import type { DictType } from '@/api/system/dict/dict-type-model';
+import type { AntdFormRules } from '@/types/form';
+import type { FormInstance } from 'antdv-next';
+
 import { computed, ref } from 'vue';
 
-import { useVbenForm } from '@/adapter/form';
 import {
   dictTypeAdd,
   dictTypeInfo,
   dictTypeUpdate,
 } from '@/api/system/dict/dict-type';
+import {
+  FormInput as Input,
+  FormTextArea as TextArea,
+} from '@/components/global/form';
 import { useVbenModal } from '@/effects/common-ui';
 import { $t } from '@/locales';
 import { cloneDeep } from '@/utils';
-import { defaultFormValueGetter, useBeforeCloseDiff } from '@/utils/popup';
-
-import { modalSchema } from './data';
+import { useBeforeCloseDiff } from '@/utils/popup';
+import { Form, FormItem } from 'antdv-next';
 
 const emit = defineEmits<{ reload: [] }>();
 
@@ -21,19 +27,46 @@ const title = computed(() => {
   return isUpdate.value ? $t('pages.common.edit') : $t('pages.common.add');
 });
 
-const [BasicForm, formApi] = useVbenForm({
-  layout: 'vertical',
-  commonConfig: {
-    labelWidth: 100,
-  },
-  schema: modalSchema(),
-  showDefaultActions: false,
+type FormData = Partial<DictType>;
+
+function getDefaultValues(): FormData {
+  return {
+    dictId: undefined,
+    dictName: '',
+    dictType: '',
+    remark: '',
+  };
+}
+
+const formData = ref<FormData>(getDefaultValues());
+const formInstance = ref<FormInstance>();
+
+const formTooltips = {
+  dictType: '使用英文/下划线命名, 如:sys_normal_disable',
+};
+
+const formRules = ref<AntdFormRules<FormData>>({
+  dictName: [{ required: true, message: $t('ui.formRules.required') }],
+  dictType: [
+    {
+      required: true,
+      message: $t('ui.formRules.required'),
+    },
+    {
+      message: '字典类型只能使用英文/下划线命名',
+      pattern: /^[a-z_]+$/i,
+    },
+  ],
 });
+
+function customFormValueGetter() {
+  return JSON.stringify(formData.value);
+}
 
 const { onBeforeClose, markInitialized, resetInitialized } = useBeforeCloseDiff(
   {
-    initializedGetter: defaultFormValueGetter(formApi),
-    currentGetter: defaultFormValueGetter(formApi),
+    initializedGetter: customFormValueGetter,
+    currentGetter: customFormValueGetter,
   },
 );
 
@@ -52,7 +85,11 @@ const [BasicModal, modalApi] = useVbenModal({
     isUpdate.value = !!id;
     if (isUpdate.value && id) {
       const record = await dictTypeInfo(id);
-      await formApi.setValues(record);
+      formData.value = {
+        ...getDefaultValues(),
+        ...record,
+        remark: record.remark ?? '',
+      };
     }
     await markInitialized();
 
@@ -63,11 +100,8 @@ const [BasicModal, modalApi] = useVbenModal({
 async function handleConfirm() {
   try {
     modalApi.lock(true);
-    const { valid } = await formApi.validate();
-    if (!valid) {
-      return;
-    }
-    const data = cloneDeep(await formApi.getValues());
+    await formInstance.value?.validate();
+    const data = cloneDeep(formData.value);
     await (isUpdate.value ? dictTypeUpdate(data) : dictTypeAdd(data));
     resetInitialized();
     emit('reload');
@@ -80,13 +114,29 @@ async function handleConfirm() {
 }
 
 async function handleClosed() {
-  await formApi.resetForm();
+  formData.value = getDefaultValues();
+  formInstance.value?.resetFields();
   resetInitialized();
 }
 </script>
 
 <template>
   <BasicModal :title="title">
-    <BasicForm />
+    <Form layout="vertical" ref="formInstance" :model="formData">
+      <FormItem label="字典名称" name="dictName" :rules="formRules.dictName">
+        <Input allow-clear class="w-full" v-model:value="formData.dictName" />
+      </FormItem>
+      <FormItem
+        label="字典类型"
+        name="dictType"
+        :rules="formRules.dictType"
+        :tooltip="formTooltips.dictType"
+      >
+        <Input allow-clear class="w-full" v-model:value="formData.dictType" />
+      </FormItem>
+      <FormItem label="备注" name="remark">
+        <TextArea allow-clear class="w-full" v-model:value="formData.remark" />
+      </FormItem>
+    </Form>
   </BasicModal>
 </template>
