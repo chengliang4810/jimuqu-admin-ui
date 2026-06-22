7a198272d1673ee3ca4b5611a6c944bd63d34c25
dap <15891557205@163.com>
2026-06-22 15:42:48 +0800

    refactor(user): 优化搜索栏展开折叠按钮样式

--- src/views/system/user/user-search.vue
+++ src/views/system/user/user-search.vue
@@ -8,9 +8,10 @@ import { FormInput, FormSelect } from '@/components/global/form';
 import { SearchButtonGroup } from '@/components/table';
 import { tableSeachClass } from '@/components/vxe-table';
 import { DictEnum } from '@/constants';
+import { ChevronDown } from '@/icons-app';
 import { formatDateTime } from '@/utils';
 import { getDictOptions } from '@/utils/dict';
-import { Card, DateRangePicker, Form, FormItem, Space } from 'antdv-next';
+import { Card, DateRangePicker, Form, FormItem } from 'antdv-next';
 
 const emit = defineEmits<{
   reset: [];
@@ -102,17 +103,19 @@ defineExpose({
           </FormItem>
         </template>
         <!-- [grid-column-end:-1] 始终定位到最后一列，justify-self-end 靠右对齐 -->
-        <div class="[grid-column-end:-1] justify-self-end">
-          <Space>
-            <a-button @click="toggleCollapse">
-              {{
-                searchCollapsed
-                  ? $t('pages.common.expand')
-                  : $t('pages.common.collapse')
-              }}
-            </a-button>
-            <SearchButtonGroup @reset="handleReset" @submit="handleSubmit" />
-          </Space>
+        <div class="[grid-column-end:-1] flex items-center justify-end gap-4">
+          <a-button type="link" @click="toggleCollapse">
+            {{
+              searchCollapsed
+                ? $t('pages.common.expand')
+                : $t('pages.common.collapse')
+            }}
+            <ChevronDown
+              class="size-4 transition-transform"
+              :class="{ 'rotate-180': !searchCollapsed }"
+            />
+          </a-button>
+          <SearchButtonGroup @reset="handleReset" @submit="handleSubmit" />
         </div>
       </div>
     </Form>
